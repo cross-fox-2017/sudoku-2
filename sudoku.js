b108314fcd23b4e7616478dbec1cb14ec064604b
Your Name <name@example.com>
2017-01-08 23:37:47 +0700

    Solve Sudoku2 With Back Tracking Recursive

--- sudoku.js
+++ sudoku.js
@@ -11,7 +11,108 @@ class Sudoku {
         this._ulang = 0;
     }
 
+    solve() {
+        for (var i = 0; i < 9; i++) {
+            for (var j = 0; j < 9; j++) {
+                if (this._papan[i][j] == 0) {
+                    this._tampung = []; //Reset Tampung Jadi Kosong
+                    var k = 0;
+                    var p = 0;
+                    //Mengambil data this._board_string Dalam Bentuk Horizontal dan Vertikal
+                    while (k < this._papan[0].length) {
+                        this._tampung.push(this._papan[i][p]) //Horizontal
+                        this._tampung.push(this._papan[p][j]) //Vertikal
+                        k++
+                        p++
+                    }
 
+                    // Mengambil data this._board_string dalam bentuk 3x3
+
+                    if (i < 3 && j < 3) {
+                        for (var m = 0; m < 3; m++) {
+                            for (var n = 0; n < 3; n++) {
+                                this._tampung.push(this._papan[m][n])
+                            }
+                        }
+                    } else if (i < 3 && j < 6) {
+                        for (var m = 0; m < 3; m++) {
+                            for (var n = 0; n < 3; n++) {
+                                this._tampung.push(this._papan[m][n + 3])
+                            }
+                        }
+                    } else if (i < 3 && j < 9) {
+                        for (var m = 0; m < 3; m++) {
+                            for (var n = 0; n < 3; n++) {
+                                this._tampung.push(this._papan[m][n + 6])
+                            }
+                        }
+                    } else if (i < 6 && j < 3) {
+                        for (var m = 0; m < 3; m++) {
+                            for (var n = 0; n < 3; n++) {
+                                this._tampung.push(this._papan[m + 3][n])
+                            }
+                        }
+                    } else if (i < 6 && j < 6) {
+                        for (var m = 0; m < 3; m++) {
+                            for (var n = 0; n < 3; n++) {
+                                this._tampung.push(this._papan[m + 3][n + 3])
+                            }
+                        }
+                    } else if (i < 6 && j < 9) {
+                        for (var m = 0; m < 3; m++) {
+                            for (var n = 0; n < 3; n++) {
+                                this._tampung.push(this._papan[m + 3][n + 6])
+                            }
+                        }
+                    } else if (i < 9 && j < 3) {
+                        for (var m = 0; m < 3; m++) {
+                            for (var n = 0; n < 3; n++) {
+                                this._tampung.push(this._papan[m + 6][n])
+                            }
+                        }
+                    } else if (i < 9 && j < 6) {
+                        for (var m = 0; m < 3; m++) {
+                            for (var n = 0; n < 3; n++) {
+                                this._tampung.push(this._papan[m + 6][n + 3])
+                            }
+                        }
+                    } else if (i < 9 && j < 99) {
+                        for (var m = 0; m < 3; m++) {
+                            for (var n = 0; n < 3; n++) {
+                                this._tampung.push(this._papan[m + 6][n + 6])
+                            }
+                        }
+                    }
+
+                    // Mengecek Menungkinan Nilai Yang Muncul
+                    var tampungNilai = []
+                    for (var l = 1; l < 10; l++) {
+                        if (this._tampung.indexOf(l.toString()) == -1) {
+                            tampungNilai.push(l.toString())
+                        }
+                    }
+                    if(tampungNilai.length==0){
+                      // console.log(this._papan);
+                      // this._ulang++
+                      // console.log(this._ulang);
+                      this._papan = []
+                      this.board()
+                      this.solve()
+                    }
+
+                    // Memasukkan Nilai ke dalam Array Kosong
+                    if (tampungNilai.length == 1) {
+                        this._papan[i][j] = tampungNilai[0]
+                    } else if (tampungNilai.length > 1) {
+                        var acak = Math.floor(Math.random() * tampungNilai.length)
+                        this._papan[i][j] = tampungNilai[acak]
+                    }
+                }
+            }
+        }
+        //Mencetak Hasil
+        return this._papan
+    }
 
     board() {
         // console.log("Sebelum :");
